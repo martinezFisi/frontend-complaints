657ad922c61a19806fe779be1692f8f810a75e09
Antony Martinez <antonymartinez12@gmail.com>
2022-07-31 19:46:53 -0500

    Added register and modal for Register Complaint Form

--- js/registerComplaint.js
+++ js/registerComplaint.js
@@ -1,3 +1,6 @@
+let HOST = "http://localhost:8282";
+let URI = "/complaints/api/v1/complaints";
+let REGISTER_COMPLAINT_ENDPOINT = HOST + URI;
 let map;
 let marker;
 let latitude;
@@ -38,6 +41,7 @@ function dragEnd(){
     var lng = marker.getPosition().lng();
     console.log("lat=" + lat + ", lng=" + lng);
     setLatLng(lat, lng);
+    setLatLngInForm(lat, lng);
     //callApiGeoAddress(lat, lng);
 }
 
@@ -45,5 +49,73 @@ function setLatLng(lat, lng){
     latitude = lat;
     longitude = lng;
 }
+
+function setLatLngInForm(lat, lng){
+    $("#inputLatitude").val(lat);
+    $("#inputLongitude").val(lng);
+}
+
+function registerComplaint(){
+
+    let complaint = {
+        address : $("#inputAddress").val(),
+        latitude : $("#inputLatitude").val(),
+        longitude : $("#inputLongitude").val(),
+        complaintType : $("#inputComplaintType").val(),
+        commentary : $("#inputCommentary").val(),
+        citizenId: 1
+    }
+
+    let complaintJson = JSON.stringify(complaint);
+    console.log("Data: " + complaintJson);
+
+    $.ajax({
+        type: "POST",
+        url: REGISTER_COMPLAINT_ENDPOINT,
+        contentType: "application/json",
+        accept: "application/json;",
+        data: complaintJson,
+        success: function (result) {
+            console.log("Se recibió respuesta del servicio");
+
+            var json = JSON.stringify(result);
+            console.log("Respuesta="+json);
+
+        },
+        error: function (jqXHR, exception) {
+            let msg = '';
+
+            switch (jqXHR.status) {
+                case 400:
+                    let resp = JSON.parse(jqXHR.responseText);
+                    msg = arrayToString(resp.errors);
+                    break;
+                case 500:
+                    msg = "Error interno en el servidor";
+                    break;
+                default:
+                    msg = "Error desconocido";
+            }
+
+            let myModal = document.getElementById('registerComplaintModal');
+            let bootstrapModal = new bootstrap.Modal(myModal, {backdrop: true})
+            let modalTitle = myModal.querySelector('#registerComplaintModalTitle');
+            let modalBody = myModal.querySelector('#registerComplaintModalBody');
+            modalTitle.textContent = "Error al registrar la denuncia";
+            modalBody.textContent = msg;
+            bootstrapModal.show();
+        }
+    });
+}
+
+function arrayToString(array) {
+    let string = '';
+    for (let value of array) {
+        string += value + '. ';
+    }
+    return string;
+}
+
+$("#registerComplaintButton").click(registerComplaint);
 
 window.initMap = initMap;
