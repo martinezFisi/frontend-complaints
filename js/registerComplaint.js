278768f9cf3000f98574be84f6d20278e8246ec4
Antony Martinez <antonymartinez12@gmail.com>
2022-08-14 23:55:02 -0500

    Added dropdown user profile and Google One Tap sign in

--- js/registerComplaint.js
+++ js/registerComplaint.js
@@ -221,6 +221,29 @@ function deleteMarkers() {
     complaintMarkers = [];
 }
 
+window.handleCredentialResponse = (response) => {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    let responsePayload = parseJwt(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+}
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
 $("#registerComplaintButton").click(registerComplaint);
 
 window.initMap = initMap;
